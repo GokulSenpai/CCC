8a72dcce8fbeb7a6cfa547fe9aa384db65b2f1cd
Gokul <gokulsenpai@gmail.com>
2020-03-20 00:57:28 +0530

    Implemented Smooth Mouse Camera

---
+++ Custom Character Controller Unity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Adds Rigidbody component automatically to the object this script is attached to 
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float gravity = 9.8f;
+    public float jumpHeight = 3f;
+    public float moveSpeed = 14f;
+
+    public GameObject cameraForRotation;
+
+    new Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = cameraForRotation.transform.right * horizontalInput + cameraForRotation.transform.forward * verticalInput;
+
+
+        rigidbody.MovePosition(moveDirection * moveSpeed * Time.deltaTime);
+        //rigidbody.MovePosition(cameraForRotation.transform.localPosition + moveDirection * Time.deltaTime * moveSpeed);
+
+        //Jump
+        //if(Input.GetButtonDown("Jump"))
+        //{
+        //    rigidbody.AddForce(transform.up * Mathf.Sqrt(2 * gravity * jumpHeight), ForceMode.VelocityChange);
+        //}
+    }
+}
---
+++ Custom Character Controller Unity/Assets/Scripts/SmoothMouseLook.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothMouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public float smoothRotateSpeed = 5f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        yRotation += mouseX;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        Quaternion currentRotation = transform.localRotation;
+        Quaternion desiredRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+
+        transform.localRotation = Quaternion.Slerp(currentRotation, desiredRotation, Time.deltaTime * smoothRotateSpeed);
+    }
+}
