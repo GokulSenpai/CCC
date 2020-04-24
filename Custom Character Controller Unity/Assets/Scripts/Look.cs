a57c870ad8d4c7fbee03887859ae5b3417624010
Gokul <gokulsenpai@gmail.com>
2020-04-24 15:08:19 +0530

    Reworked Camera and Input
    Implemented Run, Jump, Walk, Gravity, Drag using a Character Controller

---
+++ Custom Character Controller Unity/Assets/Scripts/Look.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public float smoothRotateSpeed = 5f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    public Transform playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        yRotation += mouseX;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+    }
+
+    // LateUpdate is called after all Update functions have been called.
+    void LateUpdate()
+    {
+        Quaternion currentRotation = transform.rotation;
+        Quaternion desiredRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+
+        transform.rotation = Quaternion.Slerp(currentRotation, desiredRotation, Time.smoothDeltaTime * smoothRotateSpeed);
+
+        Quaternion currentPlayerRotation = playerBody.transform.rotation;
+        Quaternion desiredPlayerRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        // Add or remove xRotation here to see a cool effect.
+
+        playerBody.transform.rotation = Quaternion.Slerp(currentPlayerRotation, desiredPlayerRotation, Time.smoothDeltaTime * smoothRotateSpeed);
+        // playerBody.transform.rotation = Quaternion.Euler(0f, playerBody.transform.rotation.y, playerBody.transform.rotation.z);
+    }
+}
---
+++ Custom Character Controller Unity/Assets/Scripts/Move.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.CompanyName.GameName
+{
+    public class Move : MonoBehaviour
+    {
+
+        #region Variables
+
+        public float speed = 7f;
+        public float sprint = 10f;
+        public float gravity = -9.81f;
+        public float jumpHeight = 3f;
+        public float groundDistance = 0.4f;
+
+        public Transform groundCheck;
+        public LayerMask groundMask;
+
+        public Camera mainCam;
+
+        public Vector3 Drag;
+
+        private bool iAmGrounded;
+
+        private float walkFOV;
+        private float runFOVMultiplier = 1.28f;
+        private float horizontalInput;
+        private float verticalInput;
+
+        private CharacterController _controller;
+        private Vector3 _velocity;
+        private Vector3 moveDirection;
+
+        #endregion
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            walkFOV = mainCam.fieldOfView;
+            _controller = GetComponent<CharacterController>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Input Axes
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+
+            // Controls
+            bool sprintingInput = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+            bool jumpingInput = Input.GetButtonDown("Jump");
+
+            // States
+            bool iAmGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+            bool iWantToJump = jumpingInput && iAmGrounded;
+            bool iWantToSprint = sprintingInput && iAmGrounded && verticalInput > 0;
+
+            // Move in the pointed direction
+            moveDirection = (transform.right * horizontalInput + transform.forward * verticalInput).normalized;
+            _controller.Move(moveDirection * Time.smoothDeltaTime * speed);
+
+            // Sprint
+            if (iWantToSprint)
+            {
+                mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, walkFOV * runFOVMultiplier, Time.smoothDeltaTime * 7f);
+                speed = sprint;
+            }
+            else
+            {
+                mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, walkFOV, Time.smoothDeltaTime * 7f);
+                speed = 6.0f;
+            }
+
+            // Gravity
+            if (iAmGrounded && _velocity.y < 0)
+            {
+                _velocity.y = -2f;
+            }
+
+            _velocity.y += gravity * Time.smoothDeltaTime;
+
+            // Jump
+            if (iWantToJump)
+            {
+                _velocity.y = Mathf.Sqrt(-2f * gravity * jumpHeight);
+            }
+
+            // Drag
+            _velocity.x /= 1 + Drag.x * Time.smoothDeltaTime;
+            _velocity.y /= 1 + Drag.y * Time.smoothDeltaTime;
+            _velocity.z /= 1 + Drag.z * Time.smoothDeltaTime;
+
+            // Move with all the forces applied
+            _controller.Move(_velocity * Time.smoothDeltaTime);
+        }
+    }
+}
+
+
+
